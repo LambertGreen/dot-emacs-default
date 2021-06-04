851b87c7ffc73e6d429179da5ea22471ef5ef85d
Lambert Green <lambert_green@msn.com>
2021-06-04 13:01:58 -0700

    Add user info and visual settings

--- init.el
+++ init.el
@@ -1,36 +1,42 @@
 ;; My personal emacs 'default' config.
-(show-paren-mode 1)
-(setq default-directory "~/")
 
-(load-theme 'tango-dark)
+(setq user-full-name "Lambert Green"
+      user-mail-address "lambert.green@gmail.com")
+
+;; Visuals
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(menu-bar-mode -1)          ; Disable the menu bar
+(set-fringe-mode 10)        ; Give some breathing room
+
+;; Theme
+(load-theme 'wombat)
+
+;; Font
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
 
 ;; Initialize package sources
 (require 'package)
-
-(setq package-arcives '(("melpa" . "https://melpa.org/packages/")
-			("org" . "https://orgmode.org/elpa/")
-			("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
-  (package-refresh-contents))
+ (package-refresh-contents))
 
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+   (package-install 'use-package))
 
 (require 'use-package)
-(setq use-package-alwayas-ensure t)
+(setq use-package-always-ensure t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)

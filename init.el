68d4a58cd2fab2917a2a2ec86c930903d09ebb1d
Lambert Green <lambert_green@msn.com>
2021-06-06 13:19:37 -0700

    Add the google-this package

--- init.el
+++ init.el
@@ -145,13 +145,17 @@
   :diminish
  )
 
+(use-package google-this
+  :config
+  (google-this-mode 1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-surround evil-goggles evil-expat evil-visualstar evil-replace-with-register evil-exchange evil-commentary evil-lion evil-collection which-key rainbow-delimiters swiper doom-modeline ivy use-package evil)))
+   '(google-this evil-surround evil-goggles evil-expat evil-visualstar evil-replace-with-register evil-exchange evil-commentary evil-lion evil-collection which-key rainbow-delimiters swiper doom-modeline ivy use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

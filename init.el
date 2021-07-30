863afacffd910b5df58041f6338c80edfb596613
Lambert Green <lambert_green@msn.com>
2021-07-29 21:11:33 -0700

    Adds a transparency command

--- init.el
+++ init.el
@@ -165,3 +165,10 @@
 (use-package magit)
 
 ;; (use-package dired-narrow)
+
+;; Set transparency of emacs
+ (defun transparency (value)
+   "Sets the transparency of the frame window. 0=transparent/100=opaque"
+   (interactive "nTransparency Value 0 - 100 opaque:")
+   (set-frame-parameter (selected-frame) 'alpha value))
+

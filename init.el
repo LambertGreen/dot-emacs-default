af42f3f261976fba0f46b3542f23ac471594f73f
Lambert Green <lambert_green@msn.com>
2023-01-14 09:34:25 -0800

    Refactor function name

--- init.el
+++ init.el
@@ -208,7 +208,7 @@
 (use-package dired-narrow)
 
 ;; Set transparency of emacs
-(defun transparency (value)
+(defun lgreen/transparency (value)
   "Sets the transparency of the frame window. 0=transparent/100=opaque"
   (interactive "nTransparency Value 0 - 100 opaque:")
   (set-frame-parameter (selected-frame) 'alpha value))

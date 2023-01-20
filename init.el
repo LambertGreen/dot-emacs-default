b45ac4228a447c9fbfce88be730a9b20389f1145
Lambert Green <lambert_green@msn.com>
2023-01-20 12:03:21 -0800

    Keep the menubar
    It's pretty useful.

--- init.el
+++ init.el
@@ -32,7 +32,6 @@
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
-(menu-bar-mode -1)          ; Disable the menu bar
 (set-fringe-mode 10)        ; Give some breathing room
 
 ;; Font

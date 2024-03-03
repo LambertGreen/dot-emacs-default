1fd15584adc5a57b393e8b906c7f3b94bf8530f8
Lambert Green <lambert_green@msn.com>
2024-03-02 23:17:32 -0800

    Adds Dashboard config

--- config/init-dashboard.el
+++ config/init-dashboard.el
@@ -2,9 +2,23 @@
 
 (use-package dashboard
   :ensure t
+  :custom
+  (dashboard-center-content t)
+  (dashboard-startup-banner 'logo)
+  (dashboard-vertically-center-content t)
+  (dashboard-display-icons-p t)
+  (dashboard-icon-type 'nerd-icons)
+  (dashboard-set-heading-icons t)
+  (dashboard-set-file-icons t)
+  (dashboard-projects-switch-function 'projectile-persp-switch-project)
+  (dashboard-items '((recents   . 5)
+		     (projects  . 5)
+		     (bookmarks . 5)
+		     (registers . 5)))
   :hook
   ((elpaca-after-init . dashboard-insert-startupify-lists)
    (elpaca-after-init . dashboard-initialize))
+
   :config
   (dashboard-setup-startup-hook))
 

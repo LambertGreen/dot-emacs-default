bd9a91958826a28506f6f2a79ae640f9e2ff127e
Lambert Green <lambert_green@msn.com>
2024-03-04 23:43:11 -0800

    Eval package: centaur-tabs

--- config/init-workspaces.el
+++ config/init-workspaces.el
@@ -12,5 +12,21 @@
 (use-package persp-projectile
   :ensure t)
 
+;; TODO Try this package out
+;; (use-package centaur-tabs
+;;   :ensure t
+;;   :custom
+;;   (centaur-tabs--buffer-show-groups t)
+;;   (centaur-tabs-cycle-scope 'groups)
+;;   :bind
+;;   (:map evil-normal-state-map
+;;         ("g t" . centaur-tabs-forward)
+;;         ("g T" . centaur-tabs-backward))
+;;   :config
+;;   (centaur-tabs-mode t)
+;;   (centaur-tabs-group-by-projectile-project)
+;;   (centaur-tabs-headline-match)
+;;   )
+
 ;;; _
 (provide 'init-workspaces)

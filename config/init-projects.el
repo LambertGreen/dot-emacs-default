f0fd9e17297406d5af99d643df8388d546944f57
Lambert Green <lambert_green@msn.com>
2024-03-09 13:59:11 -0800

    Updates projectile config
    1. Reduce search path depth, to improve performance.
    2. Add project root ignore function to exclude repos that are
    auto-setup by build and package manager tools.

--- config/init-projects.el
+++ config/init-projects.el
@@ -2,9 +2,26 @@
 
 (use-package projectile
   :ensure t
+  :custom
+  ;; Set search path and depth
+  (projectile-project-search-path '(("~/dev" . 5)))
+  (projectile-globally-ignored-files '(".DS_Store" "Icon" "TAGS"))
+  (projectile-globally-ignored-file-suffixes '(".elc" ".pyc" ".o"))
+  (projectile-ignored-projects
+   (append
+    '("~/dev/my/dotfiles/emacs/dot-emacs.default/elpaca/repos/"
+      )))
+
+  ;; NOTE: If a project is already in the cache, then one has to manually
+  ;; delete the cache file to have the project ignored.
+  ;; This can be done using the following elisp command: (delete-file projectile-cache-file)
+  (projectile-ignored-project-function
+   (lambda (project-root)
+     (or (string-prefix-p "~/dev/work/_bazel/" project-root)
+	 (string-prefix-p "/usr/lib/node_modules/" project-root))))
+
   :config
-  (projectile-mode +1)
-  (setq projectile-project-search-path '(("~/dev" . 7))))
+  (projectile-mode +1))
 
 ;;; _
 (provide 'init-projects)

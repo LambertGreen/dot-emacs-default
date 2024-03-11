307ca2acc2a172142d05c889088a5089ac8b837d
Lambert Green <lambert_green@msn.com>
2024-03-11 13:10:12 -0700

    Adds package: treesit-auto

--- config/init-prog.el
+++ config/init-prog.el
@@ -1,5 +1,13 @@
 ;;; init-prog.el --- -*- lexical-binding: t; -*-
 
+(use-package treesit-auto
+  :ensure t
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
 (use-package cmake-mode
   :ensure t
   :mode (("CMakeLists\\.txt\\'" . cmake-mode)

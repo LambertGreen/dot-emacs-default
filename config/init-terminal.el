086e26ac8289a9b6fb7dd9db03558c162775d29b
Lambert Green <lambert_green@msn.com>
2024-04-08 09:56:28 -0700

    Dsiplay vterm buffer at bottom

--- config/init-terminal.el
+++ config/init-terminal.el
@@ -6,9 +6,22 @@
 ;;; VTerm-toggle
 (use-package vterm-toggle
   :after general
+  :custom
+  (vterm-toggle-fullscreen-p nil)
   :init
   (lgreen/leader-keys
-      "o t" '(vterm-toggle :wk "Open vterm")))
+    "o t" '(vterm-toggle :wk "Open vterm"))
+  :config
+  (add-to-list 'display-buffer-alist
+	       '((lambda (buffer-or-name _)
+		   (let ((buffer (get-buffer buffer-or-name)))
+		     (with-current-buffer buffer
+		       (or (equal major-mode 'vterm-mode)
+			   (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+		 (display-buffer-reuse-window display-buffer-at-bottom)
+		 (reusable-frames . visible)
+		 (window-height . 0.4)))
+  )
 
 ;;; EAT
 (use-package eat

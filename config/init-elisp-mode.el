dfa730d6462d238af8699a54b1c8137d784ae7fa
Lambert Green <lambert_green@msn.com>
2024-04-14 12:41:20 -0700

    Adds emacs-lisp-mode keybinds

--- config/init-elisp-mode.el
+++ config/init-elisp-mode.el
@@ -3,7 +3,15 @@
 (use-package emacs-lisp-mode
   :ensure nil ; since emacs-lisp-mode is built-in
   :hook (emacs-lisp-mode . hs-minor-mode)
-  )
+  :init
+  (lgreen/local-leader-keys
+    :states 'normal
+    :keymaps 'emacs-lisp-mode-map
+    "e" '(:ignore t :wk "eval")
+    "e b" 'eval-buffer
+    "e d" 'eval-defun
+    "e e" 'eval-last-sexp
+    "e r" 'eval-region))
 
 ;;; _
 (provide 'init-elisp-mode)

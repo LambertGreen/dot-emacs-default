d1c57f3542180c9aceee4f3cf080570bb067203a
Lambert Green <lambert_green@msn.com>
2024-04-15 15:34:41 -0700

    Adds syntax for elisp
    We add an entry for "-" to consider it part of the word, so that we
    can press '*' on a identifier like "emacs-lisp-mode" and the whole
    variable will be selected.

--- config/init-elisp-mode.el
+++ config/init-elisp-mode.el
@@ -2,7 +2,6 @@
 
 (use-package emacs-lisp-mode
   :ensure nil ; since emacs-lisp-mode is built-in
-  :hook (emacs-lisp-mode . hs-minor-mode)
   :init
   (lgreen/local-leader-keys
     :states 'normal
@@ -11,7 +10,14 @@
     "e b" 'eval-buffer
     "e d" 'eval-defun
     "e e" 'eval-last-sexp
-    "e r" 'eval-region))
+    "e r" 'eval-region)
+  (defun lgreen/modify-syntax-entry-for-lisp()
+    "Adjust syntax table for lisp programming modes."
+    (modify-syntax-entry ?- "w")
+    (modify-syntax-entry ?/ "w"))
+  :hook
+  (emacs-lisp-mode . hs-minor-mode)
+  (emacs-lisp-mode . lgreen/modify-syntax-entry-for-lisp))
 
 ;;; _
 (provide 'init-elisp-mode)

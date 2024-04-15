3ed3505252c8fe05e65cc7ebe29449a76247d313
Lambert Green <lambert_green@msn.com>
2024-04-15 14:03:13 -0700

    Adds some org mode bindings

--- config/init-org.el
+++ config/init-org.el
@@ -8,6 +8,47 @@
   :custom
   (org-return-follows-link t)
   (org-blank-before-new-entry '((heading . true) (plain-list-item . true)))
+  :init
+  (lgreen/local-leader-keys
+    :states 'normal
+    :keymaps 'org-mode-map
+    "t" '(org-todo :wk "todo")
+
+    "c" '(:ignore t :wk "clock")
+    "c c" 'org-clock-cancel
+    "c d" '(:ignore t :wk "clock-display")
+    "c d d" 'org-clock-display
+    "c d r" 'org-clock-remove-overlays
+    "c i" 'org-clock-in
+    "c l" 'org-clock-in-last
+    "c o" 'org-clock-out
+    "c g" 'org-clock-goto
+    "c r" 'org-clock-report
+
+    "d" '(:ignore t :wk "deadline/date")
+    "d d" 'org-deadline
+    "d s" 'org-schedule
+    "d t" 'org-time-stamp
+    "d T" 'org-time-stamp-inactive
+
+    "b" '(:ignore t :wk "tables")
+    "b d" '(:ignore t :wk "delete")
+    "b d c" 'org-table-delete-column
+    "b d r" 'org-table-kill-row
+    "b i" '(:ignore t :wk "insert")
+    "b i c" 'org-table-insert-column
+    "b i h" 'org-table-insert-hline
+    "b i r" 'org-table-insert-row)
+
+  (defun lgreen/setup-org-calendar-navigation ()
+    (general-def
+     :keymaps 'org-read-date-minibuffer-local-map
+     :states '(normal insert)
+     "C-k" (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-week 1)))
+     "C-j" (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-week 1)))
+     "C-l" (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-day 1)))
+     "C-h" (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-day 1)))))
+  :hook (org-mode . lgreen/setup-org-calendar-navigation)
   :config
   (require 'org-tempo))
 

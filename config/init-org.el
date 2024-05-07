907956c481eab66dcec937ec1a41ebff036529c9
Lambert Green <lambert_green@msn.com>
2024-05-06 23:45:40 -0700

    Adds org config

--- config/init-org.el
+++ config/init-org.el
@@ -2,12 +2,44 @@
 
 
 ;;; Org
-;; Taming the chaos
+;; My life in plain text.
 (use-package org
-  :ensure nil
   :custom
+  (org-modules '(org-capture org-habit org-tempo))
   (org-return-follows-link t)
   (org-blank-before-new-entry '((heading . true) (plain-list-item . true)))
+  (org-todo-keywords
+   '((sequence
+      "TODO(t!)"  ; A task that needs doing & is ready to do
+      "PROJ(p!)"  ; A project, which usually contains other tasks
+      "LOOP(r!)"  ; A recurring task
+      "STRT(s!)"  ; A task that is in progress
+      "WAIT(w@/!)"  ; Something external is holding up this task
+      "HOLD(h@/!)"  ; This task is paused/on hold because of me
+      "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+      "|"
+      "DONE(d!)"  ; Task successfully completed
+      "KILL(k@/!)") ; Task was canceled, aborted or is no longer applicable
+     (sequence
+      "[ ](T)"   ; A task that needs doing
+      "[-](S)"   ; Task is in progress
+      "[?](W)"   ; Task is being held up or paused
+      "|"
+      "[X](D)")  ; Task was completed
+     (sequence
+      "|"
+      "OKAY(o)"
+      "YES(y)"
+      "NO(n)")))
+  ;; Log DONE with timestamp
+  (org-log-done 'time)
+  ;; Log state changes into drawer
+  (org-log-into-drawer t)
+  (org-agenda-log-mode-items '(closed clock state))
+  ;; FIXME Tried using this to fix treesitter org-agenda performance issue
+  ;; :hook
+  ;; but this fails with treesit-mode undefined
+  ;; (org-mode . (lambda () (treesit-mode -1)))
   :init
   (lgreen/local-leader-define-key
     :states 'normal
@@ -42,15 +74,81 @@
 
   (defun lgreen/setup-org-calendar-navigation ()
     (general-def
-     :keymaps 'org-read-date-minibuffer-local-map
-     :states '(normal insert)
-     "C-k" (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-week 1)))
-     "C-j" (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-week 1)))
-     "C-l" (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-day 1)))
-     "C-h" (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-day 1)))))
+      :keymaps 'org-read-date-minibuffer-local-map
+      :states '(normal insert)
+      "C-k" (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-week 1)))
+      "C-j" (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-week 1)))
+      "C-l" (lambda () (interactive) (org-eval-in-calendar '(calendar-forward-day 1)))
+      "C-h" (lambda () (interactive) (org-eval-in-calendar '(calendar-backward-day 1)))))
+
   :hook (org-mode . lgreen/setup-org-calendar-navigation)
   :config
-  (require 'org-tempo))
+  (org-load-modules-maybe t)
+  ;; To speed up startup, the below is specifically not put in the init section
+  (setq   org-directory "~/dev/my/org"
+	  org-agenda-files (append (directory-files-recursively (expand-file-name "personal" org-directory) "\\.org$")
+				   (directory-files-recursively (expand-file-name "work" org-directory) "\\.org$"))
+	  lgreen/org-capture-todo-file (expand-file-name "personal/todo.org" org-directory)
+	  lgreen/org-capture-notes-file (expand-file-name "personal/notes.org" org-directory)
+	  lgreen/org-capture-journal-file (expand-file-name "personal/journal.org" org-directory)
+	  lgreen/org-capture-contacts-file (expand-file-name "personal/contacts.org" org-directory)
+	  org-capture-templates
+	  '(("t" "todo" entry
+	     (file+headline lgreen/org-capture-todo-file "Inbox")
+	     "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:FROM: %a\n:END:\n" :prepend t)
+	    ("n" "notes" entry
+	     (file+headline lgreen/org-capture-notes-file "Inbox")
+	     "* %u %?\n:PROPERTIES:\n:CREATED: %U\n:FROM: %a\n:END:\n" :prepend t)
+	    ("m" "email" entry
+	     (file+olp lgreen/org-capture-todo-file "Inbox")
+	     "* TODO Mail:%u %?\n%i\n%a" :prepend t)
+	    ("j" "Journal" entry
+	     (file+olp+datetree lgreen/org-capture-journal-file)
+	     "* %U %?\n%i\n%a" :prepend t)
+	    ("p" "Protocol" entry
+	     (file+headline lgreen/org-capture-notes-file "Inbox")
+	     "* %? [[%:link][%:description]] \nCaptured On: %U\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
+	    ("L" "Protocol Link" entry
+	     (file+headline lgreen/org-capture-notes-file "Inbox")
+	     "* %? [[%:link][%:description]] \nCaptured On: %U")))
+  )
+
+;;; Evil-Org
+;; Taming the chaos with HKJL
+(use-package evil-org
+  :after (evil org)
+  :hook ((org-mode . evil-org-mode)
+	 (evil-org-mode . evil-org-set-key-theme))
+  :config
+  (evil-org-set-key-theme '(textobjects insert navigation additional shift leader todo heading))
+)
+
+;;; Evil-Org-Agenda
+;; making evil plans
+;; NOTE The `SPC' key gets overridden. I tried without sufficient success
+;; to bring back `SPC' as leader key, but it never worked 100% consistently
+;; and so I finally decided to rather keep a simpler config, and just not
+;; have a leader key while the agenda window is active.
+(use-package evil-org-agenda
+  :ensure nil
+  :after (evil-org org-agenda)
+  ;; :hook (org-agenda-mode . lgreen/org-agenda-mode-setup)
+  :config
+  (evil-org-agenda-set-keys)
+  ;; (defun lgreen/org-agenda-mode-setup ()
+  ;;   ;; Ensure keys are defined correctly for default and motion states.
+  ;;   (general-def
+  ;;     :keymaps 'org-agenda-mode-map
+  ;;     "SPC" nil
+  ;;     "<tab>" 'org-agenda-show-and-scroll-up
+  ;;     "RET" 'org-agenda-goto)
+  ;;   (general-def
+  ;;     :keymaps 'org-agenda-mode-map
+  ;;     :states 'motion
+  ;;     "SPC" nil
+  ;;     "<tab>" 'org-agenda-show-and-scroll-up
+  ;;     "RET" 'org-agenda-goto))
+  )
 
 ;;; Org-Indent
 ;; Line things up better
@@ -78,16 +176,18 @@
   :hook (org-mode . org-pretty-table-mode)
   )
 
-;;; Evil-Org
-;; Taming the chaos with HKJL
-(use-package evil-org
+;;; Org-Contacts
+;; Keeping connections
+(use-package org-contacts
   :after org
-  :hook ((org-mode . evil-org-mode)
-	 (evil-org-mode . evil-org-set-key-theme))
-  :config
-  (evil-define-key 'normal evil-org-mode-map
-    (kbd "M-k") #'org-metaup
-    (kbd "M-j") #'org-metadown))
+  :custom (org-contacts-files (list (expand-file-name "personal/contacts.org" org-directory)
+				    (expand-file-name "personal/contacts_imported_from_gmail.org" org-directory))))
+
+;;; Org-Checklist
+;; Auto-reset of checkbox state for repeating items
+;; FIXME This package is not found by Elpaca
+;; (use-package org-checklist
+;;   :after org)
 
 ;;; _
 (provide 'init-org)

1c39fb30294f25031d6031d5fa1cd85e6b1b7160
Lambert Green <lambert_green@msn.com>
2024-06-03 01:11:49 -0700

    Increase `perfect-margin` margin size

--- config/init-ui.el
+++ config/init-ui.el
@@ -167,7 +167,7 @@
 ;; Give yourself some breathing room
 (use-package perfect-margin
   :custom
-  (perfect-margin-visible-width 128)
+  (perfect-margin-visible-width 130)
   :config
   ;; enable perfect-mode
   (perfect-margin-mode t)
@@ -177,9 +177,9 @@
   ;; (setq perfect-margin-ignore-regexps nil)
   ;; add additinal bding on margin area
   (dolist (margin '("<left-margin> " "<right-margin> "))
-  (global-set-key (kbd (concat margin "<mouse-1>")) 'ignore)
-  (global-set-key (kbd (concat margin "<mouse-3>")) 'ignore)
-  (dolist (multiple '("" "double-" "triple-"))
+    (global-set-key (kbd (concat margin "<mouse-1>")) 'ignore)
+    (global-set-key (kbd (concat margin "<mouse-3>")) 'ignore)
+    (dolist (multiple '("" "double-" "triple-"))
       (global-set-key (kbd (concat margin "<" multiple "wheel-up>")) 'mwheel-scroll)
       (global-set-key (kbd (concat margin "<" multiple "wheel-down>")) 'mwheel-scroll))))
 

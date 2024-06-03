7e8a2a063e5db59362ea49e7da70b857688dedb4
Lambert Green <lambert_green@msn.com>
2024-06-03 01:12:56 -0700

    Increase the font size

--- config/init-emacs.el
+++ config/init-emacs.el
@@ -17,7 +17,7 @@
   (setq custom-file (make-temp-file "emacs-custom"))
 
   ;; Font
-  (set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 140)
+  (set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 160)
 
   ;; Visuals
   (scroll-bar-mode -1)        ; Disable visible scrollbar

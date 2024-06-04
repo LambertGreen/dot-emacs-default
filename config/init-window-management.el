2740b4e3dc32eb9bf389ab07cc52abb141719d70
Lambert Green <lambert_green@msn.com>
2024-06-03 19:49:30 -0700

    Add a replacement for `vterm` using `eat`

--- config/init-window-management.el
+++ config/init-window-management.el
@@ -9,5 +9,30 @@
     "w r" 'winner-redo
     "w u" 'winner-undo))
 
+(use-package popper
+  :bind (("C-'"   . popper-toggle)
+	 ("C-M-'"   . popper-cycle))
+  :init
+  (lgreen/leader-define-key
+    "`" '(popper-toggle :wk "Toggle popup"))
+  (setq popper-reference-buffers
+	'("\\*eat\\*"
+	  "\\*.*-eat\\*"
+	  eshell-mode    ;eshell as popups
+	  shell-mode     ;shell as popups
+	  term-mode      ;term as popups
+	  vterm-mode     ;vterm as popups
+	  help-mode      ;help windows
+	  compilation-mode)) ;compilation buffers
+  (setq popper-group-function #'popper-group-by-project)
+  (popper-mode +1)
+  (popper-echo-mode +1))
+
+;;; Shackle
+;; keep em secure
+(use-package shackle
+  :config
+  (shackle-mode 1))
+
 ;;; _
 (provide 'init-window-management)

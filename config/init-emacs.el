30a4ea98f4eb4b2b12a4336bb283da632a19f271
Lambert Green <lambert_green@msn.com>
2024-06-05 01:24:38 -0700

    Add the zapping of trailing whitespace

--- config/init-emacs.el
+++ config/init-emacs.el
@@ -172,5 +172,16 @@
 ;; Give it your best shot
 (use-package dumb-jump)
 
+;;; Whitespace-Mode
+;; Getting red in the face for the trailing space
+(use-package whitespace-mode
+  :ensure nil
+  :init
+  (setq whitespace-style '(face trailing))
+  (global-whitespace-mode 1)
+  :config
+  (custom-set-faces
+   '(whitespace-trailing ((t (:background "red"))))))
+
 ;;; _
 (provide 'init-emacs)

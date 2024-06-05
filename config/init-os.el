7c8c2041ee5dfb78f5b0e552b5742710de5cafc1
Lambert Green <lambert_green@msn.com>
2024-06-05 01:08:41 -0700

    Add config for brew site-lisp and info

--- config/init-os.el
+++ config/init-os.el
@@ -6,7 +6,23 @@
   :if (memq window-system '(mac ns))
   :config
   (setq insert-directory-program "gls"
-	dired-use-ls-dired t))
+	dired-use-ls-dired t)
 
+  (defvar homebrew-prefix)
+  (if (file-directory-p "/opt/homebrew/")
+      (setq homebrew-prefix "/opt/homebrew/")
+    (setq homebrew-prefix "/usr/local/"))
+
+  ;; Add Homebrew Emacs site-lisp to load-path
+  (let ((default-directory (concat homebrew-prefix "share/emacs/site-lisp")))
+    (normal-top-level-add-subdirs-to-load-path)))
+
+(use-package emacs
+  :ensure nil
+  :after info
+  :if (memq window-system '(mac ns))
+  :config
+  ;; Add Homebrew Info to Info path
+  (add-to-list `Info-directory-list (concat homebrew-prefix "share/info/")))
 ;;; _
 (provide 'init-os)

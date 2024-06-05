2af692abfed4bd838a48db15d7b7775021562fce
Lambert Green <lambert_green@msn.com>
2024-06-05 01:09:27 -0700

    Enable mouse support in terminal mode

--- config/init-os.el
+++ config/init-os.el
@@ -24,5 +24,23 @@
   :config
   ;; Add Homebrew Info to Info path
   (add-to-list `Info-directory-list (concat homebrew-prefix "share/info/")))
+
+(use-package emacs
+  :ensure nil
+  :config
+  ;; Enable mouse support in terminal
+  (unless window-system
+    (require 'mouse)
+    (xterm-mouse-mode t)
+    (global-set-key [mouse-4] (lambda ()
+				(interactive)
+				(scroll-down 1)))
+    (global-set-key [mouse-5] (lambda ()
+				(interactive)
+				(scroll-up 1)))
+    (defun track-mouse (_))
+    (setq mouse-sel-mode t)
+    )
+  )
 ;;; _
 (provide 'init-os)

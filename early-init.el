7702031d5bc49a41c230e784006df6dd7426fe3e
Lambert Green <lambert.green@gmail.com>
2024-06-08 13:30:37 -0700

    Add info on using No-Titlebar config

--- early-init.el
+++ early-init.el
@@ -10,3 +10,9 @@
 (add-hook 'after-init-hook
 	  (lambda ()
 	    (setq gc-cons-threshold (car (get 'gc-cons-threshold 'standard-value)))))
+
+;;; No Titlebar
+;; Some UI elements we want to set sooner rather than later
+;; Uncomment the below to remove the titlebar
+;; (add-to-list 'default-frame-alist '(undecorated . t))
+;; (add-to-list 'default-frame-alist '(undecorated-round . t))

67c619b40be9d99399a01cdf26cbf2e49e4166e0
Lambert Green <lambert.green@gmail.com>
2024-06-09 18:01:43 -0700

    Enable magit-diff-hide-trailing-cr-characters

--- config/init-version-control.el
+++ config/init-version-control.el
@@ -7,6 +7,7 @@
   :after general
   :custom
   (magit-diff-refine-hunk t)
+  (magit-diff-hide-trailing-cr-characters t)
   (magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
   :init
   (lgreen/leader-define-key

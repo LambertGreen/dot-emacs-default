f27850bfd18d236c2ad1ccba175f384709a39b61
Lambert Green <lambert.green@gmail.com>
2024-06-22 11:33:29 -0700

    Add Madrid to world-clock list

--- config/init-utils.el
+++ config/init-utils.el
@@ -40,6 +40,7 @@
   (world-clock-time-format "%a %d %b %l:%M %p %Z")
   (world-clock-list '(("America/Los_Angeles" "Seattle")
 		      ("UTC" "UTC")
+		      ("Europe/Madrid" "Madrid")
 		      ("Africa/Nairobi" "Cape Town")
 		      ("Asia/Calcutta" "New Delhi")
 		      ("Asia/Tokyo" "Tokyo"))))

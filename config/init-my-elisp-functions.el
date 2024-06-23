e882fb5a5c996dc5304a2e924ff31a574858e8ad
Lambert Green <lambert.green@gmail.com>
2024-06-22 21:17:08 -0700

    Adds function to remove background in terminal mode
    To enable the Terminal transparency to be visible in Emacs.

--- config/init-my-elisp-functions.el
+++ config/init-my-elisp-functions.el
@@ -12,6 +12,14 @@
     (interactive "nTransparency Value 0 - 100 opaque:")
     (set-frame-parameter (selected-frame) 'alpha value))
 
+  ;; Function to make the background transparent when running in a terminal
+  (defun lgreen/remove-background-in-terminal (&optional frame)
+    "Unsets the background color in terminal mode."
+    (interactive)
+    (or frame (setq frame (selected-frame)))
+    (unless (display-graphic-p frame)
+      (set-face-background 'default "unspecified-bg" frame)))
+
   ;; Insert the current filename at point
   (defun lgreen/insert-current-filename ()
     "Insert the current file name at the point."

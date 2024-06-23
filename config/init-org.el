d74a319966347465e5be78b7f2a5f7d617902961
Lambert Green <lambert.green@gmail.com>
2024-06-23 13:03:50 -0700

    Org heading font size tweaks

--- config/init-org.el
+++ config/init-org.el
@@ -178,9 +178,9 @@
     ;; Set faces for heading levels
     (dolist (face '((org-document-title . 1.5)
 		    (org-level-1 . 1.3)
-                    (org-level-2 . 1.2)
-                    (org-level-3 . 1.05)
-                    (org-level-4 . 1.0)
+                    (org-level-2 . 1.25)
+                    (org-level-3 . 1.20)
+                    (org-level-4 . 1.15)
                     (org-level-5 . 1.1)
                     (org-level-6 . 1.1)
                     (org-level-7 . 1.1)

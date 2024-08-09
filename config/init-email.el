220173373fdd2e38210d89d8aff652517b094ac1
Lambert Green <lambert.green@gmail.com>
2024-08-09 14:10:26 -0700

    Fix error trying to load mu4e on systems without mu

--- config/init-email.el
+++ config/init-email.el
@@ -6,7 +6,8 @@
 (use-package mu4e
   :ensure nil
   :after org
-  :if (not (eq system-type 'windows-nt))
+  :if (and (not (eq system-type 'windows-nt))
+           (executable-find "mu"))
   :custom
   (mu4e-index-cleanup nil)
   (mu4e-index-lazy-check t)

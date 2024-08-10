a8d7735cf1838cf3b16830dedc0315880300deb2
Lambert Green <lambert.green@gmail.com>
2024-08-10 10:07:13 -0700

    Fix setting theme for all OS

--- config/init-themes.el
+++ config/init-themes.el
@@ -26,18 +26,31 @@
 (use-package emacs
   :ensure nil
   :after doom-themes
-  :if (memq window-system '(mac ns))
   :init
-  (defun lgreen/sync-theme-with-system-appearance (appearance)
-    "Load theme taking current system APPEARANCE into account."
-    (mapc #'disable-theme custom-enabled-themes)
-    (pcase appearance
-      ('light (load-theme 'doom-one-light t))
-      ('dark (load-theme 'doom-one t))))
-  (add-hook 'ns-system-appearance-change-functions  #'lgreen/sync-theme-with-system-appearance)
-  :config
-  ;; Determine current appearance and set the appropriate theme at startup
-  (lgreen/sync-theme-with-system-appearance 'ns-system-appearance))
+  (defun lgreen/set-theme-based-on-os ()
+    "Set theme based on the operating system."
+    (cond
+     ;; For macOS, sync with system appearance
+     ((memq window-system '(mac ns))
+      (defun lgreen/sync-theme-with-system-appearance (appearance)
+	"Load theme taking current system APPEARANCE into account."
+	(mapc #'disable-theme custom-enabled-themes)
+	(pcase appearance
+          ('light (load-theme 'doom-one-light t))
+          ('dark (load-theme 'doom-one t))))
+      (add-hook 'ns-system-appearance-change-functions #'lgreen/sync-theme-with-system-appearance)
+      ;; Determine current appearance and set the appropriate theme at startup
+      (lgreen/sync-theme-with-system-appearance 'ns-system-appearance))
+
+     ;; For Windows and Linux, just set a default theme
+     ((eq system-type 'windows-nt)
+      (load-theme 'doom-one-light t))  ;; Use light theme on Windows
+
+     ((eq system-type 'gnu/linux)
+      (load-theme 'doom-one t))))  ;; Use dark theme on Linux
+
+  ;; Execute the function to set the theme based on OS
+  (lgreen/set-theme-based-on-os))
 
 ;; _
 (provide 'init-themes)

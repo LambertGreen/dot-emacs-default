43b7089df078414dd6753071edadb7b082af28c6
Lambert Green <lambert.green@gmail.com>
2024-08-23 16:31:39 -0700

    Add support for running Justfile recipes

--- config/init-projects.el
+++ config/init-projects.el
@@ -17,6 +17,20 @@
 	  (list 'vc 'Git dir)
 	nil)))
 
+  (defun lgreen/get-justfile-recipes ()
+    "Get a list of recipes from the Justfile by calling `just --summary`."
+    (let ((default-directory (project-root (project-current t))))
+      (let* ((output (string-trim-right (shell-command-to-string "just --summary")))
+             (recipes (split-string output " " t)))
+	recipes)))
+
+  (defun lgreen/run-justfile-recipe ()
+    "Prompt user to select a Justfile recipe and run it."
+    (interactive)
+    (let* ((recipes (lgreen/get-justfile-recipes))
+           (recipe (completing-read "Select project task: " recipes)))
+      (compile (concat "just " recipe))))
+
   :custom
   (project-switch-commands 'project-find-file)
   (project-vc-merge-submodules nil)
@@ -27,7 +41,8 @@
     "p f" '(project-find-file :wk "Find file in project")
     "p c" '(project-compile :wk "Compile in project")
     "p d" '(project-dired :wk "Dired in project")
-    "p b" '(project-switch-to-buffer :wk "Switch buffer in project"))
+    "p b" '(project-switch-to-buffer :wk "Switch buffer in project")
+    "p t" '(lgreen/run-justfile-recipe :wk "Run a Justfile recipe"))
 
   :config (add-to-list 'project-find-functions 'lgreen/project-try-projectile))
 

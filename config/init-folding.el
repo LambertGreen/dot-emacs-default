7052bc93478908293ae86d11454fd2ab8e7885fc
Lambert Green <lambert.green@gmail.com>
2024-10-08 23:30:01 -0700

    Update folding config
    We use the evil-folding for prog-modes, and the elisp interactivity mode.  We remove the evil-fold-list for Org and
    Outshine modes, since Outshine can have outlining and code in the same buffer making things ambiguous.  For
    Org/Outline/Outshine instead use the TAB/g-TAB (terminal) keys.

--- config/init-folding.el
+++ config/init-folding.el
@@ -1,32 +1,5 @@
 ;; init-folding.el --- -*- lexical-binding: t; -*-
 
-;;; Outline
-(use-package outline
-  :ensure nil
-  :init
-  (setq evil-fold-list '(((outline-mode outline-minor-mode)
-                          :open-all outline-show-all
-			  :close-all (lambda () (outline-hide-sublevels 1))  ;; Fix: pass a level argument
-                          :toggle outline-toggle-children
-                          :open show-entry
-                          :close hide-subtree)))
-  )
-
-;;; Org
-(use-package org
-  :ensure nil
-  :config
-  (add-to-list 'evil-fold-list
-               '((org-mode)
-		 :open-all org-show-all
-		 :close-all org-overview
-		 :toggle org-cycle
-		 :open org-show-entry
-		 :close org-hide-entry
-		 :open-rec org-show-subtree
-		 :close-rec org-hide-subtree)
-               'append)
-  )
 
 ;;; Treesit-fold
 (use-package treesit-fold
@@ -34,14 +7,12 @@
   :after outline
   :hook (prog-mode . treesit-fold-mode)
   :config
-  (add-to-list 'evil-fold-list
-               '((treesit-fold-mode)
-                 :open-all treesit-fold-open-all
-                 :close-all treesit-fold-close-all
-                 :toggle treesit-fold-toggle
-                 :open treesit-fold-open
-                 :close treesit-fold-close)
-	       'append))
+  (setq evil-fold-list '(((treesit-fold-mode)
+			  :open-all treesit-fold-open-all
+			  :close-all treesit-fold-close-all
+			  :toggle treesit-fold-toggle
+			  :open treesit-fold-open
+			  :close treesit-fold-close))))
 
 ;;; Origami
 ;; Enable Vim like code folding
@@ -50,12 +21,12 @@
   :after (outline treesit-fold)
   :config
   (add-to-list 'evil-fold-list
-               '((origami-mode)
-                 :open-all origami-open-all-nodes
-                 :close-all origami-close-all-nodes
-                 :toggle origami-toggle-node
-                 :open origami-open-node
-                 :close origami-close-node)
+	       '((origami-mode)
+		 :open-all origami-open-all-nodes
+		 :close-all origami-close-all-nodes
+		 :toggle origami-toggle-node
+		 :open origami-open-node
+		 :close origami-close-node)
 	       'append))
 
 ;;; Evil-Vimish-Fold
@@ -64,13 +35,28 @@
   :after outline
   :config
   (add-to-list 'evil-fold-list
-               '((vimish-fold-mode)
-                 :open-all vimish-fold-unfold-all
-                 :close-all vimish-fold-refold-all
-                 :toggle vimish-fold-toggle
-                 :open vimish-fold-unfold
-                 :close vimish-fold-refold)
-               'append))
+	       '((vimish-fold-mode)
+		 :open-all vimish-fold-unfold-all
+		 :close-all vimish-fold-refold-all
+		 :toggle vimish-fold-toggle
+		 :open vimish-fold-unfold
+		 :close vimish-fold-refold)
+	       'append))
+
+;;; Hideshow (hs-minor-mode) for Emacs Lisp
+(use-package hideshow
+  :ensure nil
+  :hook ((emacs-lisp-mode . hs-minor-mode)
+	 (lisp-interaction-mode . hs-minor-mode))
+  :config
+  (add-to-list 'evil-fold-list
+	       '((hs-minor-mode)
+		 :open-all hs-show-all
+		 :close-all hs-hide-all
+		 :toggle hs-toggle-hiding
+		 :open hs-show-block
+		 :close hs-hide-block)
+	       'append))
 
 ;; _
 (provide 'init-folding)

1727b9f67e16a618b86c104ed4126df23d35c844
Lambert Green <lambert.green@gmail.com>
2024-10-11 16:46:25 -0700

    Enable `undo-tree-auto-save-history` with advice
    We use advice to suppress the resulting spamming of the echo area about warnings trying to load previous saves. These
    messages are ugly because of the super long filenames needed for saving the undo per all the files.

--- config/init-undo.el
+++ config/init-undo.el
@@ -7,9 +7,9 @@
   :after (evil no-littering)
   :custom
   ;; Disabling for now because it is a pretty noisy option
-  (undo-tree-auto-save-history nil)
-  ;; (undo-tree-history-directory-alist
-  ;;  `((".*" . ,(expand-file-name "undo-tree-hist/" no-littering-var-directory))))
+  (undo-tree-auto-save-history t)
+  (undo-tree-history-directory-alist
+   `((".*" . ,(expand-file-name "undo-tree-hist/" no-littering-var-directory))))
   ;; (undo-tree-enable-undo-in-region t)
   (undo-tree-visualizer-diff t)
   (undo-tree-visualizer-timestamps t)
@@ -21,6 +21,11 @@
   :bind (:map undo-tree-map
               ("C-/" . nil))
   :config
+  ;; Suppress undo-tree save messages
+  (defun lgreen/undo-tree-suppress-save-message (orig-fun &rest args)
+    (let ((inhibit-message t))  ;; Temporarily disable messages
+      (apply orig-fun args)))
+  (advice-add 'undo-tree-save-history :around #'lgreen/undo-tree-suppress-save-message)
   (global-undo-tree-mode))
 
 ;; _

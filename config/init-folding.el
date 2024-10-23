da22d5f027e344a00e6aa302391fd782c38a4ef5
Lambert Green <lambert.green@gmail.com>
2024-10-23 11:26:03 -0700

    Fix code folding
    Main change is to set `evil-fold-list` in `:init`.
    
    `origami` does not work well for Elisp, but we had already set `hs-minor-mode` for Elisp.
    
    We probably can simplify `origami` config using lambdas, but I will consider that at a later time.

--- config/init-folding.el
+++ config/init-folding.el
@@ -6,7 +6,7 @@
   :ensure (:fetcher github :repo "emacs-tree-sitter/treesit-fold")
   :after evil
   :hook (prog-mode . treesit-fold-mode)
-  :config
+  :init
   (setq evil-fold-list '(((treesit-fold-mode)
                           :open-all treesit-fold-open-all
                           :close-all treesit-fold-close-all
@@ -14,42 +14,13 @@
                           :open treesit-fold-open
                           :close treesit-fold-close))))
 
-;;; Origami
-;; Enable Vim like code folding
-(use-package origami
-  :hook (prog-mode . origami-mode)
-  :after (evil treesit-fold)
-  :config
-  (add-to-list 'evil-fold-list
-               '((origami-mode)
-                 :open-all origami-open-all-nodes
-                 :close-all origami-close-all-nodes
-                 :toggle origami-toggle-node
-                 :open origami-open-node
-                 :close origami-close-node)
-               'append))
-
-;;; Evil-Vimish-Fold
-;; Allows manual creation/deletion of free-form folds
-(use-package evil-vimish-fold
-  :after evil
-  :config
-  (add-to-list 'evil-fold-list
-               '((vimish-fold-mode)
-                 :open-all vimish-fold-unfold-all
-                 :close-all vimish-fold-refold-all
-                 :toggle vimish-fold-toggle
-                 :open vimish-fold-unfold
-                 :close vimish-fold-refold)
-               'append))
-
 ;;; Hideshow (hs-minor-mode) for Emacs Lisp
 (use-package hideshow
   :ensure nil
-  :after evil
+  :after (evil treesit-fold)
   :hook ((emacs-lisp-mode . hs-minor-mode)
          (lisp-interaction-mode . hs-minor-mode))
-  :config
+  :init
   (add-to-list 'evil-fold-list
                '((hs-minor-mode)
                  :open-all hs-show-all
@@ -57,6 +28,62 @@
                  :toggle hs-toggle-hiding
                  :open hs-show-block
                  :close hs-hide-block)
+               'append))
+
+;;; Origami and Evil Integration
+;; Enable Vim-like code folding
+(use-package origami
+  :hook (prog-mode . origami-mode)
+  :after (evil treesit-fold hideshow)
+  :init
+  ;; Define interactive functions for Origami folding operations
+  (defun lgreen/origami-open-all-nodes ()
+    "Open all folds in the current buffer using Origami."
+    (interactive)
+    (origami-open-all-nodes (current-buffer)))
+  (defun lgreen/origami-close-all-nodes ()
+    "Close all folds in the current buffer using Origami."
+    (interactive)
+    (origami-close-all-nodes (current-buffer)))
+  (defun lgreen/origami-toggle-node ()
+    "Toggle the fold at the current point using Origami."
+    (interactive)
+    (origami-toggle-node (current-buffer) (point)))
+  (defun lgreen/origami-open-node ()
+    "Open the fold at the current point using Origami."
+    (interactive)
+    (origami-open-node (current-buffer) (point)))
+  (defun lgreen/origami-open-node-recursively ()
+    "Recursively open the fold at the current point using Origami."
+    (interactive)
+    (origami-open-node-recursively (current-buffer) (point)))
+  (defun lgreen/origami-close-node ()
+    "Close the fold at the current point using Origami."
+    (interactive)
+    (origami-close-node (current-buffer) (point)))
+  ;; Add Origami to evil-fold-list with the newly created functions
+  (add-to-list 'evil-fold-list
+               '((origami-mode)
+                 :open-all   lgreen/origami-open-all-nodes
+                 :close-all  lgreen/origami-close-all-nodes
+                 :toggle     lgreen/origami-toggle-node
+                 :open       lgreen/origami-open-node
+                 :open-rec   lgreen/origami-open-node-recursively
+                 :close      lgreen/origami-close-node)
+               'append))
+
+;;; Evil-Vimish-Fold
+;; Allows manual creation/deletion of free-form folds
+(use-package evil-vimish-fold
+  :after evil
+  :init
+  (add-to-list 'evil-fold-list
+               '((vimish-fold-mode)
+                 :open-all vimish-fold-unfold-all
+                 :close-all vimish-fold-refold-all
+                 :toggle vimish-fold-toggle
+                 :open vimish-fold-unfold
+                 :close vimish-fold-refold)
                'append))
 
 ;; _

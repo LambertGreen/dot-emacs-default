2be78d0879259c50be90542a79b310f8e7220e90
Lambert Green <lambert.green@gmail.com>
2024-10-31 13:05:18 -0700

    Adds package yasnippet

--- config/init-snippets.el
+++ config/init-snippets.el
@@ -1,9 +1,17 @@
 ;; init-snippets.el --- -*- lexical-binding: t; -*-
 
+;;; Yasnippet
+;; Template power!
+(use-package yasnippet
+  :commands yas-minor-mode
+  :hook ((prog-mode text-mode) . yas-minor-mode)
+  :config
+  (yas-reload-all))
 
 ;;; Yasnippet-Snippets
+;; Collection of templates
 (use-package yasnippet-snippets
-  :hook (prog-mode . yas-minor-mode))
+  :after yasnippet)
 
 ;;; Lorem-Ipsum
 (use-package lorem-ipsum)

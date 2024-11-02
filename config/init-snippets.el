b61294f15cf77d7fc9204dd7384a7fb5097c1df2
Lambert Green <lambert.green@gmail.com>
2024-11-02 11:47:34 -0700

    Fix yasnippets config and add keybinds

--- config/init-snippets.el
+++ config/init-snippets.el
@@ -4,7 +4,17 @@
 ;; Template power!
 (use-package yasnippet
   :commands yas-minor-mode
-  :hook ((prog-mode text-mode) . yas-minor-mode)
+  :hook (((prog-mode text-mode) . yas-minor-mode)
+         (yas-minor-mode . (lambda () (yas-activate-extra-mode 'fundamental-mode))))
+  :init
+;;;; Keymaps
+  (lgreen/leader-define-key
+    :keymaps 'yas-minor-mode-map
+    "i" '(:ignore t :wk "Insert")
+    "i i" '(yas-insert-snippet :wk "insert snippet")
+    "i n" '(yas-new-snippet :wk "new snippet")
+    "i v" '(yas-visit-snippet-file :wk "visit snippet file"))
+;;;; _
   :config
   (yas-reload-all))
 

f8545b9fc015cbe15ecb0a445881e6bddea8451e
Lambert Green <lambert.green@gmail.com>
2025-02-17 13:12:58 -0800

    Adds package: dictionary

--- config/init-utils.el
+++ config/init-utils.el
@@ -76,6 +76,13 @@
   :config
   (pdf-tools-install))
 
+(use-package dictionary
+  :ensure nil
+  :custom (dictionary-server "dict.org")
+  :init
+  (lgreen/leader-define-key
+    "x d" '(dictionary-lookup-definition :wk "lookup definition")))
+
 ;;; _
 (provide 'init-utils)
 

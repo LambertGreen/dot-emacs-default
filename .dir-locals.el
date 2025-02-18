84790b9db11d8ee14a84b24abec7be4891f45b56
Lambert Green <lambert.green@gmail.com>
2025-02-18 10:43:29 -0800

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (progn (lgreen/load-project-jinx-words) (jinx-mode 1))))))

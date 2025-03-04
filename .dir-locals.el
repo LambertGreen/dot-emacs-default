f46deebbf573f686c47cdbe4edbe1a7a2353af4e
Lambert Green <lambert.green@gmail.com>
2025-03-03 20:58:27 -0800

    Spelling words updates

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "Dired modeline")
+((nil . ((jinx-dir-local-words . "Dired LambertGreen PDFs keychain modeline")
          (eval . (progn (lgreen/load-project-jinx-words))))))

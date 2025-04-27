ec4b4caa7f9595ed970b3207cab03784be2afd08
Lambert Green <lambert.green@gmail.com>
2025-04-27 11:04:18 -0700

    Spelling updates

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "Dired Eldoc LambertGreen PDFs Skynet apheleia auth authinfo cmdproxy dwim eglot filetags functionals gls gpg homebrew jenv keychain linuxbrew mdfind minibuffer modeline ns titlebar usr")
+((nil . ((jinx-dir-local-words . "Dired Eldoc LambertGreen PDFs Skynet apheleia auth authinfo cmd cmdproxy dwim eglot exe fd filetags functionals gls gpg homebrew jenv keychain linuxbrew mdfind minibuffer modeline ns os titlebar usr zsh")
          (eval . (progn (lgreen/load-project-jinx-words))))))

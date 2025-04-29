c6291bdc947281c5e99b99a6d344ca12b693680d
Lambert Green <lambert.green@gmail.com>
2025-04-29 11:27:28 -0700

    Fix theme setting taking into account emacs-mac

--- config/init-themes.el
+++ config/init-themes.el
@@ -28,28 +28,37 @@
   :ensure nil
   :after doom-themes
   :init
-;;;; Functions
+  (defvar lgreen/preferred-light-theme 'doom-one-light
+    "Theme to use in light mode.")
+  (defvar lgreen/preferred-dark-theme 'doom-one
+    "Theme to use in dark mode.")
+
+  (defun lgreen/apply-theme-based-on-appearance (appearance)
+    "Load theme based on APPEARANCE which is either 'light or 'dark.
+If APPEARANCE is not passed, query frame parameters."
+    (let ((effective-appearance
+           (or appearance
+               (frame-parameter nil 'background-mode)))) ;; fallback if nil
+      (mapc #'disable-theme custom-enabled-themes)
+      (pcase effective-appearance
+        ('light (load-theme lgreen/preferred-light-theme t))
+        ('dark (load-theme lgreen/preferred-dark-theme t)))))
+
   (defun lgreen/set-theme-based-on-os ()
     "Set theme based on the operating system."
     (cond
-     ;; For macOS, sync with system appearance
+     ;; macOS (emacs-plus or emacs-mac)
      ((memq window-system '(mac ns))
-      (defun lgreen/sync-theme-with-system-appearance (appearance)
-        "Load theme taking current system APPEARANCE into account."
-        (mapc #'disable-theme custom-enabled-themes)
-        (pcase appearance
-          ('light (load-theme 'doom-one-light t))
-          ('dark (load-theme 'doom-one t))))
-      (add-hook 'ns-system-appearance-change-functions #'lgreen/sync-theme-with-system-appearance)
-      ;; Determine current appearance and set the appropriate theme at startup
-      (lgreen/sync-theme-with-system-appearance 'ns-system-appearance))
-     ;; For Windows and Linux, just set a default theme
-     ((eq system-type 'windows-nt)
-      (load-theme 'doom-one t))
-     ((eq system-type 'gnu/linux)
-      (load-theme 'doom-one t))))
+      ;; Attach the hook
+      (add-hook 'ns-system-appearance-change-functions #'lgreen/apply-theme-based-on-appearance)
+      ;; Set initial theme based on current appearance
+      (lgreen/apply-theme-based-on-appearance (frame-parameter nil 'background-mode)))
+     ;; Windows and Linux fallback
+     ((or (eq system-type 'windows-nt)
+          (eq system-type 'gnu/linux))
+      (load-theme lgreen/preferred-dark-theme t))))
 
-  ;; Execute the function to set the theme based on OS
+  ;; Execute immediately
   (lgreen/set-theme-based-on-os))
 
 ;;; _

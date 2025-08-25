3e500430a954f4c9fa32f3dbce8eb160fd50638d
Lambert Green <lambert.green@gmail.com>
2025-08-25 10:03:46 -0700

    Fix use of unbound variable

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((jinx-dir-local-words . "Dired Eldoc LambertGreen PDFs Skynet apheleia auth authinfo cmd cmdproxy dwim eglot elpaca eshell exe fd filetags functionals gls gpg homebrew jenv keychain linuxbrew mdfind minibuffer modeline ns os titlebar usr vterm zsh")
-         (eval . (progn (lgreen/load-project-jinx-words))))))
+         (eval . (when (fboundp 'lgreen/load-project-jinx-words) (lgreen/load-project-jinx-words))))))

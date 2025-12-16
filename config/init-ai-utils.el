199f756772384987c6134ee4ebc9f044d8059550
Lambert Green <lambert.green@gmail.com>
2025-12-16 10:03:09 -0800

    Adds package: claude-code-ide

--- config/init-ai-utils.el
+++ config/init-ai-utils.el
@@ -1,7 +1,17 @@
 ;; init-ai-utils.el --- -*- lexical-binding: t; -*-
 
 ;;; Gptel
+;; I just called to... ask you
 (use-package gptel)
+
+;;; Claude-Code-Ide
+;; Hey Jarvis... I mean Claude
+(use-package claude-code-ide
+  :ensure (:fetcher github :repo "manzaltu/claude-code-ide.el")
+  :custom (claude-code-ide-terminal-backend 'eat)
+  :bind ("C-c a" . claude-code-ide-menu)
+  :config
+  (claude-code-ide-emacs-tools-setup))
 
 ;;
 (provide 'init-ai-utils)

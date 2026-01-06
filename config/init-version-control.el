bd601024a0ebc993cc368e72a38f50a13eb0c3aa
Lambert Green <lambert.green@gmail.com>
2026-01-06 18:43:21 +0000

    Removes setting magit-diff-hide-trailing-cr-characters to true
    We do want to see "bad" chars e.g. coming from Windows systems, and
    fix them on the git side of things.

--- config/init-version-control.el
+++ config/init-version-control.el
@@ -19,7 +19,6 @@
   (magit-save-repository-buffers 'dontask)
   ;; TODO Not sure of impact of `magit-auto-revert-mode' on large repo
   (magit-auto-revert-mode t)
-  (magit-diff-hide-trailing-cr-characters t)
   (magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
   :init
 ;;;; Keymaps
